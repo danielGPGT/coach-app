e65a6b943b78280e74343a0e38660d47c95eedd3
Ch1px <danglancy02@gmail.com>
2026-02-03 21:30:09 +0000

    Remove image configuration from next.config.ts, simplifying the setup by clearing remotePatterns.

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,5 @@
 import type { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: 'images.unsplash.com', pathname: '/**' },
-      { protocol: 'https', hostname: 'cdn.pixabay.com', pathname: '/**' },
-      { protocol: 'https', hostname: 'images.pexels.com', pathname: '/**' },
-    ],
-  },
-}
+const nextConfig: NextConfig = {}
 
 export default nextConfig
